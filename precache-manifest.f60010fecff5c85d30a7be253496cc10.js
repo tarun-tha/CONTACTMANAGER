ddf587869bb49ec3564e8ebbd9e968c38812647a
tarun-tha <59115626+tarun-tha@users.noreply.github.com>
2019-12-28 12:21:09 +0530

    Updates

---
+++ precache-manifest.f60010fecff5c85d30a7be253496cc10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c5c36a80b11c614e0590bd629fd21f2",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "138944172b728da51c2f",
+    "url": "/contactmanager/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "1bf280edbca1220641eb",
+    "url": "/contactmanager/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "138944172b728da51c2f",
+    "url": "/contactmanager/static/js/2.97074a1d.chunk.js"
+  },
+  {
+    "revision": "1bf280edbca1220641eb",
+    "url": "/contactmanager/static/js/main.d7862d16.chunk.js"
+  },
+  {
+    "revision": "ff2df4108ff0483df706",
+    "url": "/contactmanager/static/js/runtime-main.9067859b.js"
+  }
+]);
